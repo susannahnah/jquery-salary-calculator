3103b4791ffa1f2c4cd3f512824d96ff7ca25f99
Susannah Harris <harris.susannah@gmail.com>
2019-05-19 21:20:02 -0500

    trying to figure out how to add inputs

--- client.js
+++ client.js
@@ -1,41 +1,57 @@
 console.log('test');
 
-// $(document).ready(onReady);
-
-// function onReady() {
-// }
-
 let moneyCount = 0
 
+//create buttons that add and delete employees from list
 $(document).ready(function () {
     console.log('jQuery has been loaded');
     $('#addEmployeeButton').on('click', handleAddClick);
-    $('#employeeTableBody').on('click', '.deleteEmployeeButton' , handleDeleteClick)
+    $('#newEmployeeInfo').on('click', '.deleteEmployeeButton' , handleDeleteClick)
 });
 
+//create a function to add an employee and their info to list
 function handleAddClick() {
-    console.log('hello events!');
-    console.log($('#firstName').val());
-    console.log($('#lastName').val());
-    console.log($('#employeeId').val());
-    console.log($('#jobTitle').val());
-    console.log($('#annualSalary').val());
-    const firstIn = $('#firstName').val();
-    const lastIn = $('#lastName').val();
-    const lastIn = $('#employeeId').val();
-    const lastIn = $('#jobTitle').val();
-    const lastIn = $('#annualSalary').val();
+    console.log($('#firstNameIn').val());
+    console.log($('#lastNameIn').val());
+    console.log($('#employeeIdIn').val());
+    console.log($('#jobTitleIn').val());
+    console.log($('#annualSalaryIn').val());
+    const firstIn = $('#firstNameIn').val();
+    const lastIn = $('#lastNameIn').val();
+    const employeeIn = $('#employeeIdIn').val();
+    const jobIn = $('#jobTitleIn').val();
+    const salaryIn = $('#annualSalaryIn').val();
 
-    $('#employeeTableBody').append(` 
+
+    $('#employeeTable').append(` 
     <tr>
-        <td>` + commonIn + `</td>
-        <td>${sciIn}</td>
-        <td>
-            <button>delete</button>
+        <td>` + firstIn + `</td>
+        <td>` + lastIn + `</td>
+        <td>` + employeeIn + `</td>
+        <td>` + jobIn + `</td>
+        <td>` + "$" + salaryIn + `</td>
+        <td><button>delete</button></td>
         </td>
     </tr>
     `); 
-    $('#commonName').val('');
-    $('#sciName').val('');
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#employeeIdIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
 
+//add a counter that increases when employee money is entered:    
+    // moneyCount = moneyCount + ();
+    // $('#moneyCountDiv').text(moneyCount);
 
+    // if (moneyCount > 20000 )  {
+    //     $('#moneyCountDiv').css('color', 'red');
+    // }    
+    
+}
+
+//created delete function for delete buttons
+function handleDeleteClick() {
+    console.log('Delete clicked');
+    $(this).closest('tr').remove();
+}
